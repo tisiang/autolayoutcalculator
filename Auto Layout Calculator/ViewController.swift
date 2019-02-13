729cb3770b3602f3e434962a1252bc32572f7fa1
tisiang <33272914+tisiang@users.noreply.github.com>
2019-02-13 21:56:43 +0800

    Initia revision
    To demonstrate designing of calculator app in Main.storyboard using autolayout.

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Tey Ti Siang on 13/2/19.
+//  Copyright © 2019 Tey Ti Siang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
